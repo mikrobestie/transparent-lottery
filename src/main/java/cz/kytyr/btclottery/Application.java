e67cf86d18a658e5ff9c6fec3f4c930462f26607
Michal <michal.kytyr@gmail.com>
2015-02-19 22:45:16 +0100

    Ignore file

---
+++ src/main/java/cz/kytyr/btclottery/Application.java
@@ -0,0 +1,15 @@
+package cz.kytyr.btclottery;/*
+ * encoding="UTF-8", (greek letters ro psi: ρψ). Do not change this comment!!!
+ * Copyright 2014 © Syntea software group a.s.
+ */
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Michal
+ * @since 19.2.2015
+ */
+@SpringBootApplication
+public class Application {
+
+}
